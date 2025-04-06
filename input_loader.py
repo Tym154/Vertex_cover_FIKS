2b8402e75d6b56caf63fabe8ebe2227792b7349e
Freedom897 <rkunst5prog@gmail.com>
2025-04-06 13:10:02 +0200

    Vytvořen input loader s chabým error_handlingem, ale nejspíš funguje na zadaný inputy.

---
+++ input_loader.py
@@ -0,0 +1,22 @@
+import sys
+
+""" Načíta input, žádnej error handeling, musí se dát správný input
+aby fungoval správně, sry skillissue"""
+
+def load_input():
+    input=sys.stdin.readlines()
+
+    V=0
+    E_num=0
+    E=[]
+
+    for line in input:
+        if line[0]=="p":
+            ls1="".join(c for c in line if  c.isdecimal())
+            V=int(ls1[0])
+            E_num=int(ls1[1])
+        elif line[0]!="c":
+            ls2=line.strip().replace(" ","")
+            print(ls2)
+            E.append([int(ls2[0]), int(ls2[1])])
+    return V, E_num, E
