2032b7e1637136eabe14203ec90a1643d6f2d5c6
Tym154 <159148113+Tym154@users.noreply.github.com>
2025-04-06 13:58:39 +0200

    Adding more functions. for checking edges and for clearing nodes without neighbours

--- graph_class.py
+++ graph_class.py
@@ -2,7 +2,7 @@ class graph:
     nodes = {}
 
     # Adding a node to the nodes dictionary
-    def add_node_to_dic(self, node_index, neighbour_nodes = []):
+    def add_node_to_dir(self, node_index, neighbour_nodes = []):
         if node_index not in neighbour_nodes:
             self.nodes.update({node_index : neighbour_nodes})
         else:
@@ -17,3 +17,22 @@ class graph:
 
         for j in nodes_to_delete:
             del self.nodes[j]
+
+    # Removing all nodes that have no neighbours
+    def clear_nodes_without_neighbours(self):
+        nodes_to_delete = []
+        for i in self.nodes:
+            if not len(self.nodes[i]): # If the nodes neighbour list is empty, delete that node
+                nodes_to_delete.append(i)
+
+        for j in nodes_to_delete:
+            self.remove_node(j)
+
+    # Returs False if there are any edges. 
+    def check_if_there_are_any_edges(self):
+        for i in self.nodes:
+            if len(self.nodes[i]):
+                return False
+            
+        return True
+    
--- testing.py
+++ testing.py
@@ -3,6 +3,22 @@ from graph_class import graph
 this_is_a_graph = graph()
 
 this_is_a_graph.add_node_to_dic(1, [2,3,5])
+this_is_a_graph.add_node_to_dic(2, [])
+this_is_a_graph.add_node_to_dic(3, [2, 1])
+
 
 print(this_is_a_graph.nodes)
+
+this_is_a_graph.clear_nodes_without_neighbours()
+
+print(this_is_a_graph.nodes)
+
+print(this_is_a_graph.check_if_there_are_any_edges())
+
+this_is_a_graph.remove_node(1)
+print(this_is_a_graph.nodes)
+
+print(this_is_a_graph.check_if_there_are_any_edges())
+
+
 
