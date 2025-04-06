e921058e5daa6f3bd50fd176ee3681994e34796b
Tym154 <159148113+Tym154@users.noreply.github.com>
2025-04-06 12:12:46 +0200

    Implementing clear_nodes_without_neighbours function

--- graph_class.py
+++ graph_class.py
@@ -1,10 +1,19 @@
 class graph:
     nodes = {}
 
-    # Function to add a node to the nodes dictionary
+    # Adding a node to the nodes dictionary
     def add_node_to_dic(self, node_index, neighbour_nodes = []):
         if node_index not in neighbour_nodes:
             self.nodes.update({node_index : neighbour_nodes})
         else:
             raise Exception("Node cannot have itself as a neighbour")
-    
+        
+    # Deleting all nodes that have no neighbours
+    def clear_nodes_without_neighbours(self):
+        nodes_to_delete = []
+        for i in self.nodes:
+            if not len(self.nodes[i]): # If the nodes neighbour list is empty, delete that node
+                nodes_to_delete.append(i)
+
+        for j in nodes_to_delete:
+            del self.nodes[j]
