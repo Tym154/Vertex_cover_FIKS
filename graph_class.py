cbd3cbfc03c3a240e76197c8b5c143d44429d388
Tym154 <159148113+Tym154@users.noreply.github.com>
2025-04-08 14:19:57 +0200

    remake

--- graph_class.py
+++ graph_class.py
@@ -1,43 +1,78 @@
 class graph:
-    nodes = {}
+    vertices = {}
+    current_vertice_num = 0
+    current_edges_num = 0
 
-    # Adding a node to the nodes dictionary
-    def add_node_to_dir(self, node_index, neighbour_nodes = []):
-        if node_index in neighbour_nodes:
+    starting_vertice_num = 0
+    starting_edges_num = 0
+
+    # graph constructor
+    def __init__(self, in_edges_num, in_vertices_num, edges):
+        self.starting_vertice_num = in_vertices_num
+        self.starting_edges_num = in_edges_num
+
+        for i in edges:
+            self.add_edge(i)
+                
+    # Add vertice based on the edge
+    def add_edge(self, edge = []):
+        if len(edge) != 2:
+            raise Exception("Path E needs to have precisely 2 elements")
+        
+        if edge[0] not in self.vertices:
+            self.vertices[edge[0]] = []
+            self.current_vertice_num += 1
+        if edge[1] not in self.vertices:
+            self.vertices[edge[1]] = []
+            self.current_vertice_num += 1
+
+        here = 0
+        if edge[0] not in self.vertices[edge[1]]:
+            self.vertices[edge[1]].append(edge[0])
+            here = 1
+        if edge[1] not in self.vertices[edge[0]]:
+            self.vertices[edge[0]].append(edge[1])
+            here = 1
+        
+        if here:
+            self.current_edges_num += 1
+
+    # Add vertice based on its neighbours
+    def add_vertice(self, vertice_index, neighbours):
+        if vertice_index in neighbours:
             raise Exception("Node cannot have itself as a neighbour")  
         
-        if node_index not in self.nodes:
-            self.nodes.update({node_index : neighbour_nodes})
-        elif node_index in self.nodes and len(neighbour_nodes) == 1:
-            self.nodes[node_index].append(neighbour_nodes)
+        if vertice_index not in self.vertices:
+            self.vertices.update({vertice_index : neighbours})
+            self.current_vertice_num += 1
+            self.current_edges_num += len(neighbours)
+            for i in neighbours:
+                self.vertices[i].append(vertice_index)
         else:
-            for i in neighbour_nodes:
-                if i not in self.nodes[node_index]:
-                    self.nodes[node_index].append(i)
+            for i in neighbours:
+                if i not in self.vertices[vertice_index]:
+                    self.vertices[vertice_index].append(i)
+                    self.current_edges_num += 1
+                    
+                    self.vertices[i].append(vertice_index)
 
-    # Removing a node from a graph
-    def remove_node(self, node_index):
-        del self.nodes[node_index]
+    # Removing a vertice from a graph
+    def remove_vertice(self, vertice_index):
+        for i in self.vertices[vertice_index]:
+            self.vertices[i].remove(vertice_index)
 
-        for i in self.nodes:
-            if node_index in self.nodes[i]:
-                self.nodes[i].remove(node_index)
+        self.current_edges_num -= len(self.vertices[vertice_index])
+        del self.vertices[vertice_index]
+        self.current_vertice_num -= 1
         
-    # Deleting all nodes that have no neighbours
-    def clear_nodes_without_neighbours(self):
-        nodes_to_delete = []
-        for i in self.nodes:
-            if not len(self.nodes[i]): # If the nodes neighbour list is empty, delete that node
-                nodes_to_delete.append(i)
+    # Deleting all vertices that have no neighbours
+    def clear_vertices_without_neighbours(self):
+        vertices_to_delete = []
+        for i in self.vertices:
+            if not len(self.vertices[i]): # If the vertices neighbour list is empty, delete that vertice
+                vertices_to_delete.append(i)
 
-        for j in nodes_to_delete:
-            del self.nodes[j]
-
-    # Returs False if there are any edges. 
-    def check_if_there_are_any_edges(self):
-        for i in self.nodes:
-            if len(self.nodes[i]):
-                return False
-            
-        return True
+        for j in vertices_to_delete:
+            del self.vertices[j]
+            self.current_vertice_num -= 1
     
--- testing.py
+++ testing.py
@@ -1,24 +1,9 @@
 from graph_class import graph
+from input_loader import load_input
 
-this_is_a_graph = graph()
+V, E_num, E = load_input()
 
-this_is_a_graph.add_node_to_dic(1, [2,3,5])
-this_is_a_graph.add_node_to_dic(2, [])
-this_is_a_graph.add_node_to_dic(3, [2, 1])
-
+this_is_a_graph = graph(E_num,V,E)
 
 print(this_is_a_graph.nodes)
-
-this_is_a_graph.clear_nodes_without_neighbours()
-
-print(this_is_a_graph.nodes)
-
-print(this_is_a_graph.check_if_there_are_any_edges())
-
-this_is_a_graph.remove_node(1)
-print(this_is_a_graph.nodes)
-
-print(this_is_a_graph.check_if_there_are_any_edges())
-
-
 
